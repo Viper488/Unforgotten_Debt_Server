937113615b9435e00be9d0c6c1584f811bb6bea8
Artur Hamernik <hamernik18@gmail.com>
2020-12-19 15:32:45 +0100

    init spring

---
+++ src/main/java/com/company/UdServerApplication.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdServerApplication.class, args);
+	}
+
+}
